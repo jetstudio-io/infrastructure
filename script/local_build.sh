fab6ff1f7d3927336ca169b8595b39060cd5807c
Louis Nguyen <louis.nguyen@jetstudio.io>
2023-06-10 11:09:50 +0200

    build node base image

--- script/local_build.sh
+++ script/local_build.sh
@@ -1,4 +1,4 @@
 docker buildx build \
   --push --platform linux/amd64,linux/arm64/v8 \
-  --build-arg PHP_IMAGE=8.2-fpm-alpine --build-arg DB_EXT=mysqli \
-  -t jetstudio/php:fpm-v8-mysqli . -f Dockerfile_php
+  --build-arg PHP_IMAGE=8.2-fpm-alpine --build-arg DB_EXT=pdo_mysql \
+  -t jetstudio/php:fpm-v8-pdo . -f Dockerfile_php
